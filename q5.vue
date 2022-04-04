b71e481384c583bfed5925729809d6404ac5506d
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-04 18:27:48 +0530

    Add files via upload

---
+++ q5.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <button @click="errorhandling()">click</button>
+    </div>    
+</template>
+<script>
+export default {
+    name:"NamE",
+    methods: {
+        errorhandling(){
+         try{
+             this.test=this.value*45;
+             console.log("test",this.value);
+            }
+         catch{
+             console.log("error handled");
+            }
+        }
+    }
+}
+</script>
